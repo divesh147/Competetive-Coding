1e02b7b45558f00adaa327b4e71ca83e3172391b
Divesh Bhagat <bhagat.divesh12@gmail.com>
2018-09-17 06:55:25 +0530

    Create under_construction2.py

---
+++ under_construction2.py
@@ -0,0 +1,34 @@
+from bisect import bisect
+for t in xrange(input()):
+    stopy = raw_input().strip()
+    stopx = raw_input().strip()
+    n,m = len(stopx),len(stopy)
+    winx = [0]
+    winy = [0]
+    losex = [0]
+    losey = [0]
+    for i in xrange(n):
+        if stopx[i]=="0": winx.append(i+1)
+        else: losex.append(i+1)
+    for j in xrange(m):
+        if stopy[j]=="0": winy.append(i+1)
+        else: losey.append(i+1)
+        
+    s = ""
+    for q in xrange(input()):
+        i,j = map(int,raw_input().split())
+        WX = bisect(winx,i)-1
+        LX = bisect(losex,i)-1
+        WY = bisect(winy,j)-1
+        LY = bisect(losey,j)-1
+        m1 = (i-winx[WX])+(j)
+        m2 = (i)+(j-winy[WY])
+        m3 = (i-losex[LX])+(j)
+        m4 = (i)+(j-losey[LY])
+        print m1,m2,m3,m4
+        ans1 = min(m1,m2)
+        ans2 = min(m3,m4)
+        c = "1"
+        if ans1%2==0 and ans2%2: c = "0"
+        s += c
+    print s
