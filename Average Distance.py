5b535abf0306bf3063dc9143b9b20a33a6021837
Divesh Bhagat <bhagat.divesh12@gmail.com>
2018-10-21 11:53:59 +0530

    Create Average Distance.py
    Chefland is a grid with N rows and M columns. Each cell of this grid is either empty or contains a house. The distance between a pair of houses is the Manhattan distance between the cells containing them.
    For each d between 1 and N+M−2 inclusive, Chef wants to calculate the number of unordered pairs of distinct houses with distance equal to d.
    https://www.codechef.com/SNCK1A19/problems/AVGMAT

---
+++ Average Distance.py
@@ -0,0 +1,37 @@
+t=int(raw_input())
+for _ in range(t):
+    n,m=[int(x) for x in raw_input().split()]
+    k=n+m
+    a=[]
+    for i in range(n):
+        a.append(raw_input())
+    ans=[0]*k
+
+    for i in range(n):
+        tmp=[0]*k
+        for j in range(m):
+            ans1=[0,0]
+            ans1.extend(tmp[1:-1])
+            if j>0 and a[i][j-1]=="1":
+                ans1[1]=1
+            
+            for x in range(i+1,n):
+                if a[x][j]=="1":
+                    ans1[abs(x-i)]+=1
+
+            if a[i][j]=="1":
+                for x in range(k-1):
+                    ans[x]+=ans1[x]
+
+            for x in range(i):
+                if a[x][j]=="1":
+                    ans1[abs(x-i)]+=1
+
+            tmp=ans1[:]
+
+    for i in range(1,k-2):
+        print ans[i],
+    print ans[k-2]
+            
+                    
+    
